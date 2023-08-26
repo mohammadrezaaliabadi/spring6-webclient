05320704793c2980271f414f6eb3defba084ab31
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-26 23:16:09 +0330

    Added client impl close #1

---
+++ src/main/java/com/example/spring6webclient/client/BeerClient.java
@@ -0,0 +1,8 @@
+package com.example.spring6webclient.client;
+
+import reactor.core.publisher.Flux;
+
+public interface BeerClient {
+    Flux<String> listBeer();
+
+}
---
+++ src/test/java/com/example/spring6webclient/client/BeerClientImplTest.java
@@ -0,0 +1,26 @@
+package com.example.spring6webclient.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerClientImplTest {
+    @Autowired
+    BeerClient client;
+
+    @Test
+    void listBeer() {
+        client.listBeer().subscribe(response->{
+            System.out.println(response);
+        });
+
+        try {
+            Thread.sleep(1000l);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
