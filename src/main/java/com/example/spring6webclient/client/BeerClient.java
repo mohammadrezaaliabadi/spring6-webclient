7cefb9405b4bd975a3c1edd2075ffa9055aa444f
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-26 23:26:15 +0330

    Get list as pojo close #4

--- src/main/java/com/example/spring6webclient/client/BeerClient.java
+++ src/main/java/com/example/spring6webclient/client/BeerClient.java
@@ -1,5 +1,6 @@
 package com.example.spring6webclient.client;
 
+import com.example.spring6webclient.model.BeerDTO;
 import com.fasterxml.jackson.databind.JsonNode;
 import reactor.core.publisher.Flux;
 
@@ -9,5 +10,7 @@ public interface BeerClient {
     Flux<String> listBeer();
     Flux<Map> listBeerMap();
     Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
 
 }
--- src/main/java/com/example/spring6webclient/client/BeerClientImpl.java
+++ src/main/java/com/example/spring6webclient/client/BeerClientImpl.java
@@ -1,5 +1,6 @@
 package com.example.spring6webclient.client;
 
+import com.example.spring6webclient.model.BeerDTO;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,18 +19,24 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Flux<String> listBeer() {
-        return webClient.get().uri(BEER_PATH, String.class).retrieve().bodyToFlux(String.class);
+        return webClient.get().uri(BEER_PATH).retrieve().bodyToFlux(String.class);
     }
 
     @Override
     public Flux<Map> listBeerMap() {
-        return webClient.get().uri(BEER_PATH, Map.class).retrieve().bodyToFlux(Map.class);
+        return webClient.get().uri(BEER_PATH).retrieve().bodyToFlux(Map.class);
 
     }
 
     @Override
     public Flux<JsonNode> listBeersJsonNode() {
-        return webClient.get().uri(BEER_PATH, JsonNode.class).retrieve().bodyToFlux(JsonNode.class);
+        return webClient.get().uri(BEER_PATH).retrieve().bodyToFlux(JsonNode.class);
+
+    }
+
+    @Override
+    public Flux<BeerDTO> listBeerDtos() {
+        return webClient.get().uri(BEER_PATH).retrieve().bodyToFlux(BeerDTO.class);
 
     }
 }
