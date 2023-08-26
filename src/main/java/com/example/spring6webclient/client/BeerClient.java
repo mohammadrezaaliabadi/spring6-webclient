3f8884960fe556f3e9c8cf330af23a943e0a8b2c
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-26 23:19:48 +0330

    Get list as map close #2

--- src/main/java/com/example/spring6webclient/client/BeerClient.java
+++ src/main/java/com/example/spring6webclient/client/BeerClient.java
@@ -2,7 +2,10 @@ package com.example.spring6webclient.client;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Map;
+
 public interface BeerClient {
     Flux<String> listBeer();
+    Flux<Map> listBeerMap();
 
 }
--- src/main/java/com/example/spring6webclient/client/BeerClientImpl.java
+++ src/main/java/com/example/spring6webclient/client/BeerClientImpl.java
@@ -3,8 +3,12 @@ package com.example.spring6webclient.client;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
 @Service
 public class BeerClientImpl implements BeerClient {
+    public static final String BEER_PATH = "/api/v3/beer";
     private final WebClient webClient;
 
     public BeerClientImpl(WebClient.Builder webClientBuilder) {
@@ -13,6 +17,12 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Flux<String> listBeer() {
-        return webClient.get().uri("/api/v3/beer", String.class).retrieve().bodyToFlux(String.class);
+        return webClient.get().uri(BEER_PATH, String.class).retrieve().bodyToFlux(String.class);
+    }
+
+    @Override
+    public Flux<Map> listBeerMap() {
+        return webClient.get().uri(BEER_PATH, Map.class).retrieve().bodyToFlux(Map.class);
+
     }
 }
